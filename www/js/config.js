eac77d29f831d1355aaaa13d45ad9f7485e8c207
rtfuzz <rtfuzz@gmail.com>
2016-07-02 07:38:29 -0300

    addd files

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+
+var krms_config ={			
+	'ApiUrl' : "http://www.apetitego.com.br/merchantapp/api",	
+	'DialogDefaultTitle' : "ApetiteGO! Seu apetite bem cuidado.",
+	'pushNotificationSenderid' : "AIzaSyAY9vP_449--7JSTGDhLzk1JnES8AMn6bA",
+	'APIHasKey' : "Fuzz0f869170c3e89a9c2719ef5TheStarb080d"
+};
